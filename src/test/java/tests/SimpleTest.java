3a67fa562d170fad1ecd36b10a20cddfaf3bf0e1
eazanova <eazanova@naumen.ru>
2022-04-05 21:19:43 +0500

    add test

---
+++ src/test/java/tests/SimpleTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Title
+ *
+ * @author eazanova
+ * @since 03.04.2022
+ */
+public class SimpleTest
+{
+
+    @Test
+    public void test() {
+        Assertions.assertTrue(true);
+    }
+
+}
